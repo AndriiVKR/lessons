a689a50f5e9c327e60834e8d6d5d6ef9a05e9607
AndriiVKR <58678209+AndriiVKR@users.noreply.github.com>
2021-01-18 00:48:50 -0800

    Update MainPalindrome.java

--- src/algorithms/MainPalindrome.java
+++ src/algorithms/MainPalindrome.java
@@ -4,6 +4,7 @@ public class MainPalindrome {
     public static void main(String[] args) {
 
         boolean a = Palindrome.isPalindrome("Selenium");
+        
         System.out.println(a);
     }
 }
