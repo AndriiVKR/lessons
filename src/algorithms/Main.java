297c9d2f03c1f0cdb06c81f2d9b24acccafe6b9c
Andrii V <you@example.com>
2021-01-18 00:44:10 -0800

    some changes

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -3,8 +3,6 @@ package algorithms;
 public class Main {
     public static void main(String[] args) {
 
-
-
         String str = "S294";
 
         Algorithm alg = new Algorithm();
--- src/algorithms/MainPalindrome.java
+++ src/algorithms/MainPalindrome.java
@@ -3,8 +3,7 @@ package algorithms;
 public class MainPalindrome {
     public static void main(String[] args) {
 
-        boolean a = Palindrome.isPalindrome("ebissibe");
+        boolean a = Palindrome.isPalindrome("Selenium");
         System.out.println(a);
-
     }
 }
