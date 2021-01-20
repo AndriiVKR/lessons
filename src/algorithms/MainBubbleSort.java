0e22b8f6fdbd0c3afff710a916502dee7396f721
Andrii V <you@example.com>
2021-01-20 00:45:14 -0800

    test+algo

---
+++ src/algorithms/MainBubbleSort.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+public class MainBubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] array = {7, 1, 9, 4, 2};
+
+        BubbleSort.bubbleSort(array);
+        //test
+    }
+}
